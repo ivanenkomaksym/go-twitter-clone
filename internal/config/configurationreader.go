ad6cad44b49ae441e8eaf1709e3f7db56d68fa73
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-01-14 12:08:45 +0200

    Add logging to configurationreader.

--- internal/config/configurationreader.go
+++ internal/config/configurationreader.go
@@ -3,6 +3,7 @@ package config
 import (
 	_ "embed"
 	"encoding/json"
+	"log"
 	"os"
 )
 
@@ -13,19 +14,23 @@ func ReadConfiguration() Configuration {
 	configuration := Configuration{}
 	json.Unmarshal(appsettingsContent, &configuration)
 
-	if tweetsStorageConnectionStringEnvVar := os.Getenv("TweetsStorage__ConnectionString"); tweetsStorageConnectionStringEnvVar != "" {
+	if tweetsStorageConnectionStringEnvVar := os.Getenv("TweetsStorage:ConnectionString"); tweetsStorageConnectionStringEnvVar != "" {
+		log.Println("Overriding TweetsStorage:ConnectionString from environment variable: ", tweetsStorageConnectionStringEnvVar)
 		configuration.TweetsStorage.ConnectionString = tweetsStorageConnectionStringEnvVar
 	}
 
-	if feedsStorageConnectionStringEnvVar := os.Getenv("FeedsStorage__ConnectionString"); feedsStorageConnectionStringEnvVar != "" {
+	if feedsStorageConnectionStringEnvVar := os.Getenv("FeedsStorage:ConnectionString"); feedsStorageConnectionStringEnvVar != "" {
+		log.Println("Overriding FeedsStorage:ConnectionString from environment variable: ", feedsStorageConnectionStringEnvVar)
 		configuration.FeedsStorage.ConnectionString = feedsStorageConnectionStringEnvVar
 	}
 
-	if apiServerApplicationUrlStringEnvVar := os.Getenv("ApiServer__ApplicationUrl"); apiServerApplicationUrlStringEnvVar != "" {
+	if apiServerApplicationUrlStringEnvVar := os.Getenv("ApiServer:ApplicationUrl"); apiServerApplicationUrlStringEnvVar != "" {
+		log.Println("Overriding ApiServer:ApplicationUrl from environment variable: ", apiServerApplicationUrlStringEnvVar)
 		configuration.ApiServer.ApplicationUrl = apiServerApplicationUrlStringEnvVar
 	}
 
 	if natsUrlStringEnvVar := os.Getenv("NATSUrl"); natsUrlStringEnvVar != "" {
+		log.Println("Overriding NATSUrl from environment variable: ", natsUrlStringEnvVar)
 		configuration.NATSUrl = natsUrlStringEnvVar
 	}
 
